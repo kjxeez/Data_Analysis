a3c7fa25e097cd2cd050d3a436b074bc741a05ef
iaakopyan <szafgvsdg@gmail.com>
2023-02-20 16:05:34 +0300

    задача текстом

--- DA 2.4.py
+++ DA 2.4.py
@@ -1,8 +1,11 @@
 import math
 N=83000
 n=200
-n_mean = 9943/200
+n_mean = 9943/n
+n2_mean = 500388/n
 print(f'средний вес груза: {n_mean}')
-N_m=n*n_mean/
-print(N_m)
-print(s/math.sqrt(200))
+print(f'Общая масса грузов: {83000*n_mean}')
+D=n2_mean-n_mean**2
+#D=1/(n-1)*500388-n/(n-1)*n_mean**2
+print(f'D = {D}')
+print(f'Ст. ошибка = {math.sqrt(D)/math.sqrt(n)}')
