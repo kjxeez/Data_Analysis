95f3d081a9b35e877151f2427854779e9aac4ed6
iaakopyan <szafgvsdg@gmail.com>
2023-02-21 13:02:43 +0300

    добавила 1 строку

--- DA 2.4.py
+++ DA 2.4.py
@@ -8,4 +8,6 @@ print(f'Общая масса грузов: {83000*n_mean}')
 D=n2_mean-n_mean**2
 #D=1/(n-1)*500388-n/(n-1)*n_mean**2
 print(f'D = {D}')
-print(f'Ст. ошибка = {math.sqrt(D)/math.sqrt(n)}')
+std=math.sqrt(D)/math.sqrt(n)
+print(f'Ст. ошибка = {std}')
+print(f'Отклонение общей массы груза ({83000*n_mean}) = {std*83000}')
