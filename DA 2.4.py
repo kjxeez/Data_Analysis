5ad8d14a43ae367c8386420242be94574b93d0f0
kjxeez <82978703+kjxeez@users.noreply.github.com>
2023-02-21 13:40:31 +0300

    Update DA 2.4.py

--- DA 2.4.py
+++ DA 2.4.py
@@ -9,5 +9,6 @@ D=n2_mean-n_mean**2
 #D=1/(n-1)*500388-n/(n-1)*n_mean**2
 print(f'D = {D}')
 std=math.sqrt(D)/math.sqrt(n)
+#std=math.sqrt(D)/math.sqrt(n-1)
 print(f'Ст. ошибка = {std}')
 print(f'Отклонение общей массы груза ({83000*n_mean}) = {std*83000}')
