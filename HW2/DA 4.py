d31ca97581ccb2b98101ff7e386bff3d086b6605
iaakopyan <szafgvsdg@gmail.com>
2023-02-26 19:30:21 +0300

    Задача 6

--- HW2/DA 4.py
+++ HW2/DA 4.py
@@ -1,24 +1,22 @@
 import math
 
 N = 14000
-C = {0: 100, 1: 200, 2: 120, 3: 40, 4: 20, 5: 20}
-#95% общее колво детей в районе
-n=sum(C.get(i) for i in C)
-print(n)
-y1 = sum(i*C.get(i) for i in C)
-print(y1)
-y2 = sum(i**2*C.get(i) for i in C)
+A = {0: 100, 1: 200, 2: 120, 3: 40, 4: 20, 5: 20}
+B = {4: 501, 5: 497, 6: 2}
+N = N - sum(B.get(i) for i in B)
+n = 500 - 40
+y_mean = sum(i*A.get(i) for i in range(4))/n
+print(f'выборочное среднее: {y_mean}')
+Y = N*y_mean + sum(i*B.get(i) for i in B)
+print(f'оценка общего числа детей: {Y}')
 
-Y = N*y1/n
-print(f'оценка суммарного числа детей: {Y}')
-D=1/(n-1)*(y2-y1**2/n)
-print(f'D = {D}')
-
-o=math.sqrt(D/n*(1-n/N))
-p=0.95
-t=3
-inc = t*o
-yl = (n_mean+Ql*stdy)
-print(f'Левая граница Д.И. для среднего: {yl}')
-yr = (n_mean+Qr*stdy)
-print(f'Правая граница Д.И. для среднего: {yr}')
+y2_mean = sum(i**2*A.get(i) for i in range(4))/n
+D = n/(n-1)*(y2_mean-y_mean**2)
+std = math.sqrt(D)
+print(f'оценка среднего кв откл: {std}')
+#0.95
+Q = 1.96
+stdy = 1.96*std/math.sqrt(n)*math.sqrt(1-n/N)
+stdY = stdy*N
+print(f'Оценка ошибки общей совокупности: {stdY}')
+print(f'\033[31mДИ общего кол-ва детей в районе: {Y-stdY,Y+stdY}')
